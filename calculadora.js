2a69aa8707d268430b83d6ccf08245ec97f725a1
annaestherrr <annaesther_15@hotmail.com>
2022-02-22 15:31:28 -0300

    adicionei botão limpar

--- calculadora.js
+++ calculadora.js
@@ -1,24 +1,30 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import './App.css';
 
-export default class Calc extends Component {
+
+
+export class App extends Component {
   state = {
-    result: 0,
-    num1: null,
-    num2: null
+    n1: "null",
+    n2: "null",
+    res: "0"
   };
 
-  handleChange = (e) => {
+
+
+  handleChange2 = (event) => {
     this.setState({
-      num1: e.target.value
-    });
-  };
-  handleChange2 = (e) => {
-    this.setState({
-      num2: e.target.value
+      n2: event.target.value
     });
   };
 
+  handleChange1 = (event) => {
+    this.setState({
+      n1: event.target.value
+    });
+  };
+
+  
   minus = () => {
     let { num1, num2 } = this.state;
     this.setState({
@@ -44,24 +50,37 @@ export default class Calc extends Component {
     });
   };
 
+ clear = ()=>{
+    let {num1, num2} = this.state
+    if(num1 && num2 !== ""){
+      this.setState({
+        result: null,
+        n1:" ",
+        n2:" "
+      })
+    }else{
+      this.setState({
+        result: "Nenhum valor foi digitado"
+      })
+    }
+  }
+
   render() {
     return (
-      <div className='container'>
-        <h1>CALCULADORA LINDA REACT BY ANNA ESTHER</h1>
+      <div>
+        <h1>Calc App</h1>
         <h2>{this.state.result}</h2>
-        <div className='calc'>
-          <input className='input' onChange={this.handleChange} type="number" />
-          <input className='input' onChange={this.handleChange2} type="number" />
-         <div className='calc-button'>
-         <button className='button' onClick={this.minus}>-</button>
-         <button className='button' onClick={this.add}>+</button>
-         <button className='button' onClick={this.divide}>/</button>
-         <button className='button' onClick={this.multiply}>*</button>
-         </div>
-        
-        </div>
-       
+        <input onChange={this.handleChange1} type="number" />
+        <input onChange={this.handleChange2} type="number" />
+        <button  onClick={this.divide}>/</button>
+        <button onClick={this.multiply}>*</button>
+        <button onClick={this.minus}>-</button>
+        <button onClick={this.add}>+</button>
+        <button onClick={this.clear}>LIMPAR</button>
       </div>
     );
   }
 }
+
+
+export default App;
